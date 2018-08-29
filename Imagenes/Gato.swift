d3c7fa47d0d3169470f4a3d8c65d22f786448994
Alumno <ulsa@iMac-de-Alumno.local>
2018-08-28 18:43:19 -0700

    Vimos como utilizar gesturas, añadir secuencia de imagenes y manipulacion de tipos de datos

---
+++ Imagenes/Gato.swift
@@ -0,0 +1,18 @@
+//
+//  Gato.swift
+//  Imagenes
+//
+//  Created by Alumno on 28/08/18.
+//  Copyright © 2018 Alumno. All rights reserved.
+//
+
+import Foundation
+
+class Gato {
+    var nombre : String?
+    var vidas : Int?
+    var edad : Int?
+    var esterilizado : Bool?
+    
+    
+}
--- Imagenes/ViewController.swift
+++ Imagenes/ViewController.swift
@@ -10,9 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblTexto: UILabel!
+    @IBOutlet weak var imgSecuencia: UIImageView!
+    
+    @IBAction func doTapGato(_ sender: Any) {
+        
+    
+        //Hacer tap para cambiar etiqueta y empezar/detener secuencia de imágenes
+        if imgSecuencia.isAnimating{
+            imgSecuencia.stopAnimating()
+            lblTexto.text = "Miau"
+        }else {
+        imgSecuencia.startAnimating()
+            lblTexto.text = "Continua"
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Placeholder de la imagen detenida
+        imgSecuencia.image = UIImage (named: "0")
+        
+        //Secuencia de imágenes
+        var imagenes : [UIImage] = []
+        imagenes.append(UIImage (named: "0")!)
+        imagenes.append(UIImage (named: "1")!)
+        imagenes.append(UIImage (named: "2")!)
+        imagenes.append(UIImage (named: "3")!)
+        imagenes.append(UIImage (named: "4")!)
+        imagenes.append(UIImage (named: "5")!)
+        imagenes.append(UIImage (named: "6")!)
+        imagenes.append(UIImage (named: "7")!)
+
+        imgSecuencia.animationImages = imagenes
+        imgSecuencia.animationDuration = 0.5
+        imgSecuencia.startAnimating()
+        
+        let miGato = Gato()
+        miGato.nombre = "Garfield"
+        miGato.vidas = 3
+        
+        // ! Significa unwrapping
+        // ? Significa que puede tener un valor nulo
+       
+        //Validacion de que miGato.nombre no sea nulo
+        if miGato.nombre != nil {
+            let nombreGato : String = miGato.nombre!
+            lblTexto.text = nombreGato
+        }
+        
+        
+        lblTexto.text = miGato.nombre
     }
 
     override func didReceiveMemoryWarning() {
